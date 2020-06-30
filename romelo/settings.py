0e81650456c7dc28f2fc61bcfa8b4fb535f86c2c
thepsalmist <xf.xavierfrank@gmail.com>
2020-06-30 11:53:30 +0300

    update allowed hosts

--- romelo/settings.py
+++ romelo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "romelotech.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "209.97.140.84"]
 
 
 # Application definition

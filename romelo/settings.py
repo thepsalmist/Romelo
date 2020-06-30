f7536b1f55e466064b2e67f8c7074a3f5667bb88
thepsalmist <xf.xavierfrank@gmail.com>
2020-06-30 13:08:09 +0300

    modify settings

--- romelo/settings.py
+++ romelo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "209.97.140.84"]
+ALLOWED_HOSTS = ["127.0.0.1", "209.97.140.84", "localhost"]
 
 
 # Application definition
